79e685643fc372658267ffb80cda21f9e2a60e8f
rizk <rizkdotai@gmail.com>
2024-02-21 21:14:29 +0200

    wrap images in container

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const API_KEY = "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"
 const submitIcon = document.querySelector("#submit-icon")
-const inputElement = document.querySelector('input')
-const imageSection = document.querySelector('.images-section')
+const inputElement = document.querySelector("input")
+const imageSection = document.querySelector(".images-section")
 
 const getImages = async () => {
     const options = {
@@ -11,26 +11,25 @@ const getImages = async () => {
             "content-type": "application/json"
         },
         body: JSON.stringify({
-            "prompt" : inputElement.value,
-            "n": 4,
-            "size": "1024x1024"
+            prompt : inputElement.value,
+            n: 4,
+            size: "1024x1024"
         }) 
     }
     try {
-     const response =  await fetch('https://api.openai.com/v1/images/generations', options)
+     const response =  await fetch("https://api.openai.com/v1/images/generations", options)
      const data = await response.json()
-     console.log(data)
     } catch (error) {
         console.error(error)
     }
 
     data?.data.forEach(imageObject => {
-        const ImageContainer = document.createElement('div')
-        ImageContainer.classList.add('image-container')
-        const imageElement = document.createElement('img')
-        imageElement.setAttribute('src', imageObject.url)
-        ImageContainer.append(imageElement)
-        images-section.append(ImageContainer)
+        const imageContainer = document.createElement("div")
+        imageContainer.classList.add("image-container")
+        const imageElement = document.createElement("img")
+        imageElement.setAttribute("src", imageObject.url)
+        imageContainer.append(imageElement)
+        images-section.append(imageContainer)
 
     })
 
